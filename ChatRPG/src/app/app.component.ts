b48d072143e31c89ab8fdaca227e0170c686891d
Tamas Molnar <tamas.molnar@nagarro.com>
2023-10-23 17:12:40 +0200

    first basic frontend code, copied from other project

--- ChatRPG/src/app/app.component.ts
+++ ChatRPG/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+export class ChatMessage {
+  author: string;
+  message: string;
+
+  constructor(author: string, message: string) {
+      this.author = author
+      this.message = message
+  }
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
   title = 'ChatRPG';
+  messages: ChatMessage[] = [];
+  chatText = ""
+
+  onChatMessageSent(event: Event | undefined) {
+    if (event) event.preventDefault()
+    if (this.chatText.trim() === "") return
+    
+    this.messages.push({author:"You", message: this.chatText})
+    this.messages.push({author:"AI", message: "Interesting, I'm not sure."})
+    this.chatText = ""
+  }
 }
