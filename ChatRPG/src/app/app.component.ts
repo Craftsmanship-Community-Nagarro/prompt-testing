30010b6f648d60925e10697f96ab297bb765d3a1
Tamas Molnar <tamas.molnar@nagarro.com>
2023-10-24 16:52:21 +0200

    integrate openai client

--- ChatRPG/src/app/app.component.ts
+++ ChatRPG/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import OpenAI from 'openai';
+import ChatCompletionMessageParam  from 'openai';
 
-export class ChatMessage {
-  author: string;
-  message: string;
 
-  constructor(author: string, message: string) {
-      this.author = author
-      this.message = message
-  }
+interface ChatMessage {
+  role: 'user' | 'assistant';
+  content: string;
 }
-
 
 @Component({
   selector: 'app-root',
@@ -28,25 +25,31 @@ export class AppComponent {
     if (event) event.preventDefault()
     if (this.chatText.trim() === "") return
     
-    this.sendChatMessage(this.chatText.trim())
+    this.sendChatMessage()
     this.chatText = ""
   }
 
-  sendChatMessage(text: string) {
-    this.isLoading = true
+  sendChatMessage() {
+    this.messages.push({role:"user", content: this.chatText})
 
-    this.messages.push({author:"You", message: text})
+    this.isLoading = true
     const scrollingInterval = setInterval(() => {
       this.scrollToBottomOfChat();
     });
     
-    this.simulateStreamingResponse("Interesting, I'm not sure.")
+    const gptMessages = this.buildMessages()
+    streamChatResponse(gptMessages)
 
     setTimeout(() => clearInterval(scrollingInterval), 1000);
     this.scrollToBottomOfChat();
-
-  
     this.isLoading = false
+  }
+
+  buildMessages(): GptMessage[] {
+    const gptMessages: GptMessage[] = [];
+    gptMessages.push({role:"system", content:systemPrompt});
+    gptMessages.push(...this.messages);
+    return gptMessages;
   }
 
   scrollToBottomOfChat() {
@@ -54,22 +57,29 @@ export class AppComponent {
       top: this.chatHistory.nativeElement.scrollHeight,
     });
   }
+}
 
-  async simulateStreamingResponse(text: string) {
-    const currentMessage = new ChatMessage("AI", "");
-    this.messages.push(currentMessage);
+// This is the OpenAI GPT Integration part
 
-    const wordsArray = text.split(' ')
+const dotenv = require('dotenv');
+dotenv.config();
 
-    for (const word of wordsArray) {
-      await this.wait(100) //
-      currentMessage.message += word + " "
-    }
-  }
+const systemPrompt = "You are a friendly assistant."
 
-  wait(ms: number): Promise<void> {
-    return new Promise<void>((resolve) => {
-      setTimeout(resolve, ms);
-    });
+interface GptMessage {
+  role: 'system' | 'user' | 'assistant';
+  content: string;
+}
+
+const openai = new OpenAI();
+async function* streamChatResponse(messages: GptMessage[]) {
+  const stream = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo', // the model can be switched here
+    messages: messages,
+    stream: true, // we would like to stream back each chunk, instead of waiting for the whole response message
+  });
+  for await (const part of stream) {
+    const chunk = part.choices[0]?.delta?.content || '';
+    yield chunk;
   }
 }
--- ChatRPG/src/chat.service.ts
+++ ChatRPG/src/chat.service.ts
@@ -4,7 +4,7 @@ const openai = new OpenAI({
     apiKey: 'My API Key', // defaults to process.env["OPENAI_API_KEY"]
   });
 
-async function* streamResponse(messages: string[]) {
+async function* streamChatResponse(messages: string[]) {
   const stream = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
     messages: [{ role: 'user', content: 'Say this is a test' }],
